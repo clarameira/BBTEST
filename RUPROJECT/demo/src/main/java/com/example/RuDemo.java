0a22d6d30846a018a2af479bea7ee9c138ee70be
Clara Meira <claraa.meiraa@hotmail.com>
2024-07-25 16:42:14 -0300

    RuDemo

---
+++ RUPROJECT/demo/src/main/java/com/example/RuDemo.java
@@ -0,0 +1,9 @@
+package com.example;
+
+import com.example.Aplicações.RestauranteFacade;
+public class Main {
+    public static void main(String[] args) {
+        RestauranteFacade restauranteFacade = new RestauranteFacade();
+        restauranteFacade.iniciar();
+    }
+}
